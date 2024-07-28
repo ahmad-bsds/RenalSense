3a9d66f890212b936b482eddf7b809485c3a9459
MUHAMMAD AHMAD <ahmad.bsds@gmail.com>
2024-07-28 16:01:36 +0500

    Working virsion of bot.py

--- interface/bot.py
+++ interface/bot.py
@@ -1,4 +1,6 @@
 import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage
 from dotenv import load_dotenv
 import os
 # Load the environment variables from the .env file at the specified path
@@ -10,29 +12,23 @@ fa_api_key = os.getenv('FALCON_API_KEY')
 AI71_BASE_URL = "https://api.ai71.ai/v1/"
 AI71_API_KEY = fa_api_key
 
-client = openai.OpenAI(
+chat = ChatOpenAI(
+    model="tiiuae/falcon-180B-chat",
     api_key=AI71_API_KEY,
     base_url=AI71_BASE_URL,
+    streaming=True,
 )
 
 # Simple invocation:
-print(client.chat.completions.create(
-    model="tiiuae/falcon-11b",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"},
-    ],
-))
+print(
+    chat.invoke(
+        [
+            SystemMessage(content="You are a helpful assistant"),
+            HumanMessage(content="Hello!"),
+        ]
+    )
+)
 
 # Streaming invocation:
-for chunk in client.chat.completions.create(
-    messages=[{
-      "role": "user",
-      "content": "Write two words you like the most."
-    }],
-    model="tiiuae/falcon-11b",
-    stream=True,
-):
-    delta_content = chunk.choices[0].delta.content
-    if delta_content:
-        print(delta_content, sep="", end="", flush=True)
+for chunk in chat.stream("Write two words you most like."):
+    print(chunk.content, end="", flush=True)
